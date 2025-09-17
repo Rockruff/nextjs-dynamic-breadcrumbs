c051f617d8e80158927187d0231a49d83e3d0d53
Rockruff <36600169+Rockruff@users.noreply.github.com>
2025-09-17 14:25:08 +0800

    fix: empty href for root

--- dist/hook.js
+++ dist/hook.js
@@ -5,7 +5,7 @@ export default function useBreadcrumb() {
   const segmentsAll = usePathname().split(SEP);
   const segmentsAfter = useSelectedLayoutSegments();
   const segments = segmentsAll.slice(0, segmentsAll.length - segmentsAfter.length);
-  const href = segments.join(SEP);
+  const href = segments.join(SEP) || SEP;
   const isActive = segmentsAfter.length === 0;
   return [href, isActive];
 }
